11bb2a5f23087638eb321c6be18061b482feb2c6
tocoquinho <giovanni.morales@thunderatz.org>
2021-12-20 01:28:39 -0300

    ✨ Add data to files

--- utils/file.py
+++ utils/file.py
@@ -1,8 +1,9 @@
 class File:
-    def __init__(self, name, job_mix, size = 0):
+    def __init__(self, name, job_mix, size = 0, data = []):
         self.name = name
         self.job_mix = job_mix
         self.result = 0
+        self.data = data
 
         # set file size based on jobs size
         if size == 0:
@@ -17,4 +18,4 @@ class File:
 
     def print(self):
         print(f"File {self.name}, {self.size/1.0e3:.4}k, {self.result}")
-        job_mix.print()
+        print(f"Data: {self.data}")
